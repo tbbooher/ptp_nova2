53a532ec2ec265a9107bc73370451db89d313b5b
tim booher <tim@theboohers.org>
2013-10-08 21:50:47 -0400

    added images

---
+++ front-page.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage Lysander
+ * @since HTML5 Reset 2.0
+ */
+ get_header(); ?>
+            <!-- slideshow -->
+            <section class="featured m-all d-all">
+				<div class="slideshow d2-d11">			
+				</div>
+			</section>
+
+			<!-- buckets section -->
+			<section class="buckets m-all d-all">
+				<div class="bucket1 m-all ag2">
+					<h3><strong>We</strong> love kids</h3>
+					<p>We focus totally on kids. With over 15
+					   years of experience treating clients.
+					</p>
+					   <a class="button">LEARN MORE</a>
+					
+				</div>
+				<div class="bucket2 m-all ag6">
+					<h3><strong>We</strong> come to you</h3>
+					<p>Because there’s no better learning
+					   environment than your own home. 
+					</p>
+					<a class="button">LEARN MORE</a>
+				</div>
+				<div class="bucket3 m-all ag11">
+					<h3><strong>We</strong> make kids stronger</h3>
+					<p>Our experience and methods help to keep
+					   your kids healthy and strong. 					   
+					</p>
+					<a class="button">LEARN MORE</a>
+				</div>
+				<div class="pics d-all">
+					<div class="m-all x">
+						<figure>
+							<img src="<?php bloginfo('template_url'); ?>/images/kids.jpg">
+							<figcaption><a href="">See our experience »</a></figcaption>
+						</figure>
+					</div>
+					
+					<div class="m-all y">
+						<figure>
+							<img src="<?php bloginfo('template_url'); ?>/images/house.jpg">
+							<figcaption><a href="">Area of coverage »</a></figcaption>
+						</figure>
+					</div>
+					
+					<div class="m-all z">
+						<figure>
+							<img src="<?php bloginfo('template_url'); ?>/images/boy.jpg">
+							<figcaption><a href="">Our services »</a></figcaption>
+						</figure>
+					</div>
+				</div>
+			</section>
+			
+<?php get_footer(); ?>
