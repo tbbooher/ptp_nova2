4785ad9ea584822455db2eda94dcb381af72ad72
tim booher <tim@theboohers.org>
2013-10-08 21:45:32 -0400

    now looking like cameron

--- footer.php
+++ footer.php
@@ -7,20 +7,46 @@
  * @package ptp_NoVa
  */
 ?>
-
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'ptp_nova_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'ptp_nova' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'ptp_nova' ), 'ptp_NoVa', '<a href="http://www.theboohers.org" rel="designer">Tim Booher</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
-</body>
+	        <section class="footer m-all d-all">
+				<a href="/"><div class="f-logo d2-d4"></div></a>
+				
+				<div class="m-all">
+					<p>We're social!</p>
+				</div>
+				
+				<div class="m-all">
+					<p>Newsletter</p>
+					
+					<p>
+					Subscribe to our monthly newsletter and be the first
+					to know about our news and special deals!
+					</p>
+					<form action="" class="d7-d11">
+						<input type="text" name="email" class="email" placeholder="enter email">
+					</form>
+				</div>
+			
+			</section>
+        </div><!--/wrapper-->
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        <script>
+      var navigation = responsiveNav(".nav-collapse", {
+        animate: true,        // Boolean: Use CSS3 transitions, true or false
+        transition: 250,      // Integer: Speed of the transition, in milliseconds
+        label: "Menu",        // String: Label for the navigation toggle
+        insert: "after",      // String: Insert the toggle before or after the navigation
+        customToggle: "",     // Selector: Specify the ID of a custom toggle
+        openPos: "relative",  // String: Position of the opened nav, relative or static
+        jsClass: "js",        // String: 'JS enabled' class which is added to <html> el
+        init: function(){},   // Function: Init callback
+        open: function(){},   // Function: Open callback
+        close: function(){}   // Function: Close callback
+      });
+    </script>
+    </body>
+    <?php wp_footer(); ?>    
 </html>
--- header.php
+++ header.php
@@ -7,30 +7,39 @@
  * @package ptp_NoVa
  */
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title><?php wp_title( '|', true, 'right' ); ?></title>        
+        <meta name="description" content="Chrissy Booher is a Pediatric Physical Therapist who provides in-home Physical Therapy Evaluation and Treatments to infants and children in Northern Virginia. Therapy is play based, yet guided by research driven principles as well as a thorough understanding of anatomy, human movement and development.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<?php wp_head(); ?>
-</head>
+        <!-- fix these to pull from the main js -->
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
 
-<body <?php body_class(); ?>>
-<div class="wrapper m-all d-all">
-	<section class="header">
-		<a href="/"><div class="logo d3-d6"></div></a>
-		<div class="m-all d6-d12">
-			<nav class="nav-collapse closed" style="-webkit-transition: max-height 250ms; transition: max-height 250ms; position: relative;" aria-hidden="false">
-               <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><a href="#" class="nav-toggle" aria-hidden="true">Menu</a>
-		</div>
-	</section>
-</div>
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<div id="page" class="hfeed site">
-	<?php do_action( 'before' ); ?>
+        <?php wp_head(); ?>
 
-	<div id="content" class="site-content">
+        <link href="https://get.gridsetapp.com/23622/" rel="stylesheet" />
+        <link href='http://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:300italic,400,300' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="wrapper m-all d-all">
+        	<section class="header">
+        	    <a href="/"><div class="logo d3-d6"></div></a>
+        		<div class="m-all d6-d12">
+	        		<nav class="nav-collapse">
+                       <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+				    </nav>
+        		</div>
+        	</section>
