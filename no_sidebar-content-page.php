01a5df41531b117fb85dd8b7146954f77ad59cf7
tim booher <tim@theboohers.org>
2013-10-11 21:15:20 -0400

    added

---
+++ no_sidebar-content-page.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ Template Name: Content Page
+ * The template for displaying pages with content only. (no sidebar)
+ *
+ * @package ptp_NoVa
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area d-all m-all">
+		<main id="main" class="site-main" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template();
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</section><!-- #primary -->
+
+<?php get_footer(); ?>
