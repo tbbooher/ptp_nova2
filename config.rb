603434bda17f15f46790736527bb1a4ecdf7992d
Tim Booher <tim@theboohers.org>
2013-10-20 14:02:13 -0400

    cleaned up forms

--- config.rb
+++ config.rb
@@ -8,13 +8,29 @@ css_dir = "/"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 project_type = :stand_alone
 output_style = :expanded # :compressed # 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
+
+# # The root of all operations, must be set for Compass to work.
+# http_path             = "/"
+ 
+# # Directory where Compass dumps the generated CSS files
+# http_css_path         = http_path + "assets/css"
+ 
+# # Directory where font files use in font-face declarations are stored
+# http_fonts_path       = http_path + "assets/fonts"
+ 
+# # Directory where Compass stores the Grid image, and the sites images are stored
+# http_images_path      = http_path + "assets/img/"
+ 
+# # Directory where the sites' JavaScript file are stored
+# http_javascripts_path = http_path + "assets/js"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
