c8f2e9733da34c39e9db107775f24a0c87e1ed9d
Tim Booher <tim@theboohers.org>
2013-10-24 21:25:56 -0400

    updated config

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 project_type = :stand_alone
-output_style = :expanded # :compressed # 
+output_style = :compressed #
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
