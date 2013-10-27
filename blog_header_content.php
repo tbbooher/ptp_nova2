5b51121788cc7404a355a384adc5650292a59050
Tim Booher <tim@theboohers.org>
2013-10-27 19:08:13 -0400

    fixes #25 with a header included, might want to move this to the right

---
+++ blog_header_content.php
@@ -0,0 +1,8 @@
+<h1 style="text-transform:uppercase;">Chrissy Booher's Blog</h1>
+<i>
+Applying the latest in pediatic therapy research and practice to make children stronger,
+happier, and develop to their full potential. Your comments and participation are most
+welcome. Please let me know if I can answer any questions in the comment fields after each post
+or email me at chrissy@ptpnova.com.
+</i>
+<hr>
