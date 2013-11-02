e36e2db22df8298e80f911bdfc2d60cafc695ae3
tim booher <tim@theboohers.org>
2013-11-02 16:34:38 -0400

    added current posts only

--- footer.php
+++ footer.php
@@ -30,7 +30,7 @@
 						<em class="ribbon">BLOG POSTS</em>
 							<ul class="well with_border">
 								<?php
-								$args = array( 'numberposts' => '5' );
+								$args = array( 'numberposts' => '5', 'post_status' => 'publish' );
 								$recent_posts = wp_get_recent_posts( $args  );
 								foreach( $recent_posts as $recent ){
 									echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
