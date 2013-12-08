3e7c95c7c675ce21677a7f5009148f4427a4e880
Tim Booher <tim@theboohers.org>
2013-12-07 21:41:19 -0500

    uglified

--- js/uglify.rb
+++ js/uglify.rb
@@ -1,5 +1,5 @@
 require 'uglifier'
 
 File.open("../js.min.js", "w") do |file| 
-    file.write Uglifier.compile(`cat *.js`, output: {beautify: true, comments: :all})
+    file.write Uglifier.compile(`cat *.js`, output: {beautify: false, comments: :none})
 end
